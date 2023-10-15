2b7a256251c13d2470d32c03a16ebc1ce5a13304
svdorn <svdorn@wisc.edu>
2023-10-15 16:18:12 -0500

    change input files to be in 1 folder

--- calculate_prs.sh
+++ calculate_prs.sh
@@ -5,10 +5,10 @@ heritability=0.3
 RL=1
 
 ## genrate PRS scripts
-Rscript --vanilla ${script_path}/LDpred2.R $work_dir $heritability $RL
+Rscript --vanilla ${script_path}/LDpred2.R $work_dir $heritability
 
 ## sort PRS model weights
-Rscript --vanilla ${script_path}/sort_SNP_weights.R $work_dir $RL
+Rscript --vanilla ${script_path}/sort_SNP_weights.R $work_dir
 
 ## PRS score calculation for LD pred2
 for n in {3..13};
@@ -16,7 +16,7 @@ do
     echo
     echo "Calculating PRS scores for LDpred2 model ${n}"
     echo
-    ./plink/plink --bfile ./input/LD/1kg_hm3_QCed_noM --score ${work_dir}/ldpred2.txt 1 2 ${n} header sum center --out ${work_dir}/ldpred2.${n}
+    ./plink/plink --bfile ./input/1kg_hm3_QCed_noM --score ${work_dir}/ldpred2.txt 1 2 ${n} header sum center --out ${work_dir}/ldpred2.${n}
 done
 ## PRS score caclulation for default PRS
 for n in {3..7};
@@ -24,7 +24,7 @@ do
     echo
     echo "Calculating PRS scores for default model ${n}"
     echo
-    ./plink/plink --bfile ./input/LD/1kg_hm3_QCed_noM --score ${work_dir}/default.txt 1 2 ${n} header sum center --out ${work_dir}/default.${n}
+    ./plink/plink --bfile ./input/1kg_hm3_QCed_noM --score ${work_dir}/default.txt 1 2 ${n} header sum center --out ${work_dir}/default.${n}
 done
 
 ## combine PRS calculations into a final table
--- scripts/sort_SNP_weights.R
+++ scripts/sort_SNP_weights.R
@@ -5,7 +5,6 @@ suppressMessages(library(tidyverse))
 options(stringsAsFactors=F)
 args = commandArgs(trailingOnly=TRUE)
 wd <- as.character(args[1])
-k <- as.numeric(args[2])
 
 ####################### define helper functions ###################
 # Obtain necessary information from PRS output for PRS calculation
@@ -18,48 +17,44 @@ extract_snp_weights <- function(dat,snp,a1,weight){
 cat("\nSorting SNP weights for LDpred2...")
 # LDpred2
 params <- c(1:13)
-for (ite in 1:k){
-    for (j in 1:length(params)){
-        dat <- c()
-        weight.col <- j + 2
-        for (chr in 1:22) {
-            dat.tmp <- as.data.frame(fread(paste0(wd,"/chr",chr,".ldpred2.txt"),header=F))
-            dat.sorted <- extract_snp_weights(dat=dat.tmp,snp="V1",a1="V2",weight=weight.col)
-            dat <- rbind(dat,dat.sorted)
-        }
-        dat[,3] <- dat[,3]/sd(dat[,3])
-        if(j == 1) {
-            lddat <- dat
-        } else {
-            lddat <- cbind(lddat, dat[,3])
-        }
+for (j in 1:length(params)){
+    dat <- c()
+    weight.col <- j + 2
+    for (chr in 1:22) {
+        dat.tmp <- as.data.frame(fread(paste0(wd,"/chr",chr,".ldpred2.txt"),header=F))
+        dat.sorted <- extract_snp_weights(dat=dat.tmp,snp="V1",a1="V2",weight=weight.col)
+        dat <- rbind(dat,dat.sorted)
     }
-
-    fwrite(lddat,paste0(wd,"/ldpred2.txt"),col.names=F,row.names=F,sep="\t",quote=F)
+    dat[,3] <- dat[,3]/sd(dat[,3])
+    if(j == 1) {
+        lddat <- dat
+    } else {
+        lddat <- cbind(lddat, dat[,3])
+    }
 }
+
+fwrite(lddat,paste0(wd,"/ldpred2.txt"),col.names=F,row.names=F,sep="\t",quote=F)
 cat("\nFinished sorting SNP weights for LDpred2\n")
 
 # prs default - use beta sumstats above p-value threshold
 cat("\nRunning default PRS method and sorting SNP weights...")
 p_value_cutoff <- c(0.01, 0.05, 0.1, 0.5, 1)
-for (ite in 1:k) {
-    gwas.raw <- as.data.frame(fread(paste0("./input/gwas/gwas_RL_",ite,".txt.gz"),header=T))
-    gwas.out <- gwas.raw[,c(3,4,7,9)]
-    colnames(gwas.out) <- c("snp","a1","weight","p")
+gwas.raw <- as.data.frame(fread(paste0("./input/gwas_train.txt.gz"),header=T))
+gwas.out <- gwas.raw[,c(3,4,7,9)]
+colnames(gwas.out) <- c("snp","a1","weight","p")
 
-    for (j in 1:length(p_value_cutoff)) {
-        dat <- gwas.out
-        
-        dat <- dat %>% 
-            mutate(weight = ifelse(p <= p_value_cutoff[j], weight, 0), p = NULL)
-        
-        if(j == 1) {
-            default.dat <- dat
-        } else {
-            default.dat <- cbind(default.dat, dat[,3])
-        }
+for (j in 1:length(p_value_cutoff)) {
+    dat <- gwas.out
+    
+    dat <- dat %>% 
+        mutate(weight = ifelse(p <= p_value_cutoff[j], weight, 0), p = NULL)
+    
+    if(j == 1) {
+        default.dat <- dat
+    } else {
+        default.dat <- cbind(default.dat, dat[,3])
     }
-
-    fwrite(default.dat,paste0(wd,"/default.txt"),col.names=F,row.names=F,sep="\t",quote=F)
 }
+
+fwrite(default.dat,paste0(wd,"/default.txt"),col.names=F,row.names=F,sep="\t",quote=F)
 cat("\nFinished running default PRS method and sorting SNP weights\n")
